44e9fee196965254860e1e019dd79eb003c52816
Mior Muhammad Zaki <crynobone@gmail.com>
2020-03-21 19:50:47 +0800

    Fixes tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/JsonRpc/MathAdd.php
+++ tests/JsonRpc/MathAdd.php
@@ -2,10 +2,16 @@
 
 namespace Minions\Polyfill\Tests\JsonRpc;
 
+use Minions\Http\Request;
+
 class MathAdd
 {
-    public function __invoke(array $arguments)
+    public function __invoke($arguments)
     {
-        return \array_sum($arguments);
+        if (\is_array($arguments)) {
+            return \array_sum($arguments);
+        } elseif ($arguments instanceof Request) {
+            return \array_sum($arguments->all());
+        }
     }
 }
